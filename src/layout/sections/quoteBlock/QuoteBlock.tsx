1416d1e49dbe6e1397f9d41dd9f586af28a0b8fa
Ostrovskiy_Danila <119683894+DanilaOstrovskiy@users.noreply.github.com>
2024-11-18 15:15:23 +0300

    feature: create quote block

---
+++ src/layout/sections/quoteBlock/QuoteBlock.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import styled from "styled-components";
+
+type QuoteBlockPropsType = {
+    blockquote: string;
+    author: string;
+}
+
+export const QuoteBlock = (props: QuoteBlockPropsType) => {
+    let {blockquote, author} = props
+    return (
+
+            <StyledBlockquote>{blockquote}
+                <StyledBlockquoteFooter>{author}</StyledBlockquoteFooter>
+            </StyledBlockquote>
+
+
+    );
+};
+
+const StyledBlockquote = styled.blockquote`
+    background-color: pink;
+    max-height: 100vh;
+    text-align: center;
+`
+const StyledBlockquoteFooter = styled.footer``
+
