ff18280de92f11917d17a5872931f7b6020083da
Ostrovskiy_Danila <119683894+DanilaOstrovskiy@users.noreply.github.com>
2024-12-28 04:23:42 +0300

    feat: add container to layout and styles

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -2,37 +2,53 @@ import React from 'react';
 import styled from "styled-components";
 import photo from '../../../assets/images/avatar1.webp'
 import {FlexWrapper} from "../../../components/flexWrapper/FlexWrapper";
-import {Button} from "../../../components/button/Button";
+import {Container} from "../../../components/Container";
+import {theme} from "../../../styles/Theme";
+import {StyledButton} from "../../../components/button/Button";
 
 export const Main = () => {
     return (
         <StyledMain>
-            <FlexWrapper align={'center'} justify={'space-around'}>
-                <div>
-                    <Name>Elias is a web designer and front-end developer</Name>
-                    <MainTitle>He crafts responsive websites where technologies meet creativity</MainTitle>
-                    <Button>Contact me!!</Button>
-                </div>
-                <div>
-                    <Photo src={photo} alt=""/>
-                    <span>Currently working on Portfolio</span>
-                </div>
-            </FlexWrapper>
+            <Container>
+                <FlexWrapper align={'center'} justify={'space-around'}>
+                    <div>
+                        <Name>Elias is a web designer and front-end developer</Name>
+                        <MainTitle>He crafts responsive websites where technologies meet creativity</MainTitle>
+                        <StyledButton>Contact me!!</StyledButton>
+                    </div>
+                    <div>
+                        <Photo src={photo} alt=""/>
+                        <span>Currently working on Portfolio</span>
+                    </div>
+                </FlexWrapper>
+            </Container>
         </StyledMain>
     );
 };
+
 
 const StyledMain = styled.section`
     min-height: 100vh;
 `
 
-const Photo = styled.img`
-    width: 350px;
-    height: 350px;
-    object-fit: cover;`
-
-const MainTitle = styled.h1`
-`
-
 const Name = styled.h2`
+    font-weight: 600;
+    font-size: 32px;
 `
+const MainTitle = styled.h1`
+    font-weight: 400;
+    font-size: 16px;
+    color: ${theme.colors.secondaryText}
+`
+
+
+
+
+const Photo = styled.img`
+    width: 457px;
+    height: 386px;
+    object-fit: cover;
+    object-position: 0 60px;
+`
+
+
