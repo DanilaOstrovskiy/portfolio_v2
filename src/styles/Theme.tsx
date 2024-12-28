1de4785fe94f2952eba26153493dfab48d1e21a5
Ostrovskiy_Danila <119683894+DanilaOstrovskiy@users.noreply.github.com>
2024-12-28 04:22:10 +0300

    feat: add css styles for universal button

--- src/styles/Theme.tsx
+++ src/styles/Theme.tsx
@@ -1,8 +1,32 @@
 export const theme = {
     colors: {
-        primaryBg: "#282c33",
+        primaryBg: "#2c3036",
+        secondaryBg: "#282c33",
         accent: "#c778dd",
-        font: "#fafafa"
+        primaryText: "#fafafa",
+        secondaryText: "#abb2bf",
+        button: {
+            primary: {
+                defaultBg: "transparent",
+                hoverBg: "rgba(179, 102, 199, 0.2)",
+                activeBg: "#ae4cc6",
+                text: "#fafafa",
+                border: "#c778dd",
+            },
+            secondary: {
+                defaultBg: "transparent",
+                hoverBg: "rgba(171, 178, 191, 0.2)",
+                activeBg: "#525253",
+                text: "#abb2bf",
+                border: "#abb2bf",
+            },
+            danger: {
+                defaultBg: "#e06c75",
+                hoverBg: "#cc6169",
+                activeBg: "#b8575e",
+                text: "#fafafa",
+                border: "#ff0000",
+            }
+        }
     }
 }
-
