9a394f25d46b2ef2d5be0f0f1a351f6632b21e46
Ostrovskiy_Danila <119683894+DanilaOstrovskiy@users.noreply.github.com>
2024-12-28 04:21:43 +0300

    feat: add and structure, business logic and styles

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Icon} from "../icon/Icon";
+import styled from "styled-components";
+import {theme} from "../../styles/Theme";
 
 export const Logo = () => {
-    return (
-        <a href="">
-            <Icon iconId={"logo"}/>
-        </a>
-    );
-};
+    const [iconId, setIconId] = useState("logo");
 
+    return (
+        <Link href=""
+              onMouseEnter={() => setIconId("pink_logo")}
+              onMouseLeave={() => setIconId("logo")}>
+            <Icon width={"16px"} height={"16px"} iconId={iconId} viewBox={"0 0 53 53"} />
+            <span>Elias</span>
+        </Link>
+    );
+
+};
+const Link = styled.a`
+    &:hover {
+        color: ${theme.colors.primaryText};
+    }
+    
+    & span {
+        padding-left: 5px;
+        color: white;
+        font-weight: 500;
+    }`
