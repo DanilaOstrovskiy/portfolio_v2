87b619168cf59fd174e9739bc321158743eabd7b
Ostrovskiy_Danila <119683894+DanilaOstrovskiy@users.noreply.github.com>
2025-01-03 22:57:57 +0300

    feature: update Main section – added icons, improved styles, and updated structure

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -5,21 +5,31 @@ import {FlexWrapper} from "../../../components/flexWrapper/FlexWrapper";
 import {Container} from "../../../components/Container";
 import {theme} from "../../../styles/Theme";
 import {StyledButton} from "../../../components/button/Button";
+import {Icon} from "../../../components/icon/Icon";
+
 
 export const Main = () => {
     return (
         <StyledMain>
             <Container>
-                <FlexWrapper align={'center'} justify={'space-around'}>
+                <FlexWrapper align={'center'} justify={'space-between'}>
                     <div>
-                        <Name>Elias is a web designer and front-end developer</Name>
-                        <MainTitle>He crafts responsive websites where technologies meet creativity</MainTitle>
-                        <StyledButton>Contact me!!</StyledButton>
+                        <Name>Elias is a <span>web designer</span> and <span>front-end developer</span></Name>
+                        <MainTitle>He crafts responsive websites where technologies <br/>meet creativity</MainTitle>
+                        <StyledButton size={"small"} >Contact me !!</StyledButton>
                     </div>
-                    <div>
-                        <Photo src={photo} alt=""/>
-                        <span>Currently working on Portfolio</span>
-                    </div>
+                    <FlexWrapper direction={"column"} align={'center'}>
+                        <PhotoWrapper>
+                            <StyledIconWrapper className="logo-wrapper">
+                                <Icon iconId={"big_Logo"} viewBox={"0 0 156 156"} height={"156px"} width={"156px"}/>
+                            </StyledIconWrapper>
+                            <Photo src={photo} alt="me"/>
+                            <StyledIconWrapper className="dots-wrapper">
+                                <Icon iconId={"dots5x5"} viewBox={"0 0 84 84"} height={"84px"} width={"84px"}/>
+                            </StyledIconWrapper>
+                        </PhotoWrapper>
+                        <Status><span></span>Currently working on <b>Portfolio</b></Status>
+                    </FlexWrapper>
                 </FlexWrapper>
             </Container>
         </StyledMain>
@@ -28,27 +38,92 @@ export const Main = () => {
 
 
 const StyledMain = styled.section`
-    min-height: 100vh;
+    min-height: 60vh;
+
 `
+
 
 const Name = styled.h2`
     font-weight: 600;
     font-size: 32px;
+
+    span {
+        color: ${theme.colors.accent}
+    }
 `
 const MainTitle = styled.h1`
     font-weight: 400;
     font-size: 16px;
-    color: ${theme.colors.secondaryText}
+    color: ${theme.colors.secondaryText};
+    margin: 31px 0 24px 0;
+    line-height: 156%;
+
 `
 
 
+const PhotoWrapper = styled.div`
+    max-height: 386px;
+    max-width: 469px;
+    overflow: hidden; // обрезаем всё, что выходит за пределы
+    position: relative;
+    z-index: 0;
+`
 
+const StyledIconWrapper = styled.div`
+    position: absolute;
 
+    &.logo-wrapper {
+        z-index: -1;
+        top: 90px;
+        left: 15px;
+    }
+
+    &.dots-wrapper {
+        z-index: 1;
+        bottom: 45px;
+        right: 10px;
+    }
+`
 const Photo = styled.img`
-    width: 457px;
-    height: 386px;
+    width: 650px;
+    height: 100%;
     object-fit: cover;
-    object-position: 0 60px;
+    object-position: -115px -77px;
+`
+
+const Status = styled.span`
+    display: inline-block;
+    text-align: left;
+    position: relative;
+    z-index: 1;
+    padding: 6px 6px 6px 35px;
+    border: 1px solid ${theme.colors.secondaryText};
+    color: ${theme.colors.secondaryText};
+    max-width: 402px;
+    width: 100%;
+
+    span {
+        position: relative;
+
+        &::before {
+            content: "";
+            border: 1px solid ${theme.colors.accent};
+            background-color: ${theme.colors.accent};
+            width: 16px;
+            height: 16px;
+            position: absolute;
+            left: -25px; // отступ слева
+            top: 50%; // позиционирование по центру
+            transform: translateY(-50%); // точное центрирование
+            z-index: -1;
+        }
+    }
+
+    b {
+        font-weight: inherit;
+        color: ${theme.colors.primaryText};
+    }
+
 `
 
 
