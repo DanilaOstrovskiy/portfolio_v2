ad42ecb39ceaecdf81166a6bdf0747c6fb085543
Ostrovskiy_Danila <119683894+DanilaOstrovskiy@users.noreply.github.com>
2025-01-28 17:32:15 +0300

    refactor: -Styles moved to a separate file.ts - Change names of variables feature: -Created a hook that recognizes screen size

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,48 +1,43 @@
-import React from 'react';
-import styled from "styled-components";
+import React, {useEffect} from 'react';
 import {Logo} from "../../components/logo/Logo";
 import {FlexWrapper} from "../../components/shared/flexWrapper/FlexWrapper";
 import {Container} from "../../components/shared/Container/Container";
-import {HeaderMenu} from "./headerMenu/HeaderMenu";
-import {theme} from "../../styles/Theme";
+
 import {MobileMenu} from "./mobileMenu/MobileMenu";
+import {S} from './Header_styles'
+import {DesktopMenu} from "./desktopMenu/DesktopMenu";
+import {useWindowWidth} from "../../hooks/useWindowWidth";
 
 
 export const Header = () => {
+    const isMobile = useWindowWidth(768)
+
+
+
+
     const headerMenu = [
-        { name: 'common.menu.home', path: 'home' },
-        { name: 'common.menu.projects', path: 'works' },
-        { name: 'common.menu.about', path: 'about' },
-        { name: 'common.menu.contacts', path: 'contacts' }
+        {name: 'common.menu.home', path: 'home'},
+        {name: 'common.menu.projects', path: 'works'},
+        {name: 'common.menu.about', path: 'about'},
+        {name: 'common.menu.contacts', path: 'contacts'}
     ]
 
+
     return (
-        <StyledHeader>
+        <S.Header>
             <Container>
                 <FlexWrapper justify={'space-between'} align={'flex-end'}>
-                    <Logo />
-                    <HeaderMenu menuItems={headerMenu}/>
-                    <MobileMenu menuItems={headerMenu}/>
+                    <Logo/>
+                    {
+                        isMobile ?
+                        <MobileMenu menuItems={headerMenu}/> :
+                        <DesktopMenu menuItems={headerMenu}/>
+                    }
                 </FlexWrapper>
             </Container>
-        </StyledHeader>
+        </S.Header>
     );
 };
 
-const StyledHeader = styled.header`
-    padding-top: 32px;
-    padding-bottom: 35px;
-    background-color: ${theme.colors.primaryBg};
 
-    @media ${theme.media.tablet} {
-        padding-top: 19px;
-        padding-bottom: 8px;
-        position: fixed;
-        left: 0;
-        right: 0;
-        z-index: 99999;
-    }
-    
-    
-`
 
