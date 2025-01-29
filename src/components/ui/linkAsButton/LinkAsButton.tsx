ef51662134e29306426af1fcee9c13e4e00068ed
Danila Ostrovskiy <ostrov2ostrov2@gmail.com>
2025-01-29 19:36:49 +0300

    feat: add smooth scroll functionality to LinkAsButton for anchor links

--- src/components/ui/linkAsButton/LinkAsButton.tsx
+++ src/components/ui/linkAsButton/LinkAsButton.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {DefaultTheme} from "styled-components";
-import {LinkProps} from "react-router-dom";
+import {LinkProps, useNavigate} from "react-router-dom";
 import {ButtonSize, ButtonVariant} from "../button/Button";
 import {S} from "./LinkAsButton_styles"
 
@@ -38,16 +38,38 @@ export const LinkAsButton: React.FC<LinkAsButtonProps> = ({
                                                           }) => {
     const rel = getRelAttribute(target);
 
+    const navigate = useNavigate();
+
+    const handleClick = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+        if (to.includes("#")) {
+            event.preventDefault();
+
+            // Сначала навигируем на страницу
+            navigate(to, { replace: true });
+
+            // Затем выполняем скролл
+            const id = to.split('#')[1];
+            const element = document.getElementById(id);
+
+            if (element) {
+                setTimeout(() => {
+                    element.scrollIntoView({ behavior: "smooth" });
+                }, 100);
+            }
+        }
+    };
+
     return (
         <S.StyledLink
-            to={to}
-            disabled={disabled}
-            target={target}
-            rel={rel}
-            variant={variant}
-            size={size}
-            fullWidth={fullWidth}
-            {...props}
+                      to={to}
+                      disabled={disabled}
+                      target={target}
+                      rel={rel}
+                      variant={variant}
+                      size={size}
+                      fullWidth={fullWidth}
+                      onClick={handleClick}
+                      {...props}
         >
             {children}
         </S.StyledLink>
@@ -55,6 +77,5 @@ export const LinkAsButton: React.FC<LinkAsButtonProps> = ({
 };
 
 
-
 // Экспорт типов для использования в других компонентах
-export type { LinkAsButtonProps, BaseStyleProps };
+export type {LinkAsButtonProps, BaseStyleProps};
