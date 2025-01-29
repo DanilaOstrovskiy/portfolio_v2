e103d39a53aca9225054d4b6073c3834ea3470bf
Danila Ostrovskiy <ostrov2ostrov2@gmail.com>
2025-01-29 19:42:44 +0300

    feat: add smooth scroll behavior for anchor links
    - Implemented `useEffect` to handle smooth scrolling when navigating to sections via anchor links
    - Used `useLocation` from `react-router-dom` to detect URL hash and trigger scroll into view
    - Added `id="works"` to `SectionHeader` to ensure correct scrolling target

--- src/pages/works/Works.tsx
+++ src/pages/works/Works.tsx
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {CompleteApps} from "./completeApps/CompleteApps";
 import {SmallProjects} from "./smallProjects/SmallProjects";
 import SectionHeader from "./sectionHeader/SectionHeader";
 import {useTranslation} from "react-i18next";
 import {S} from "./Works_styles"
+import {useLocation} from "react-router-dom";
 
 
 const Works = () => {
     const {t} = useTranslation();
+    const location = useLocation();
+
+    useEffect(() => {
+        if (location.hash) {
+            const id = location.hash.substring(1); // Убираем #
+            const element = document.getElementById(id);
+
+            if (element) {
+                setTimeout(() => { // Даем время DOM загрузиться
+                    element.scrollIntoView({ behavior: "smooth" });
+                }, 100);
+            }
+        }
+    }, [location]);
+
     return (
         <S.Works>
-            <SectionHeader prefix={"/"} title={t('projects.title')}  description={t('projects.description')} />
+            <SectionHeader id={"works"} prefix={"/"} title={t('projects.title')}  description={t('projects.description')} />
             <CompleteApps/>
             <SmallProjects/>
         </S.Works>
