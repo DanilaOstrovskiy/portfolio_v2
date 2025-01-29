365281cac37119bdf7bdf1112632cac76f041dbb
Danila Ostrovskiy <ostrov2ostrov2@gmail.com>
2025-01-29 19:33:48 +0300

    fix: add padding-top

--- src/pages/about/AboutMe_styles.ts
+++ src/pages/about/AboutMe_styles.ts
@@ -2,7 +2,7 @@ import {theme} from "../../styles/Theme";
 import styled from "styled-components";
 
 const AboutMe = styled.section`
-
+    padding-top: 32px;
     @media ${theme.media.mobile}, ${theme.media.tablet} {
         padding: 32px 20px 12px;
     }`
