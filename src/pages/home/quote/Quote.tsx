274fde859f9c12f8301d970b1a08030e78e56275
Danila Ostrovskiy <ostrov2ostrov2@gmail.com>
2025-01-29 19:37:12 +0300

    fix: minor changes

--- src/pages/home/quote/Quote.tsx
+++ src/pages/home/quote/Quote.tsx
@@ -14,27 +14,27 @@ export const Quote = () => {
         <S.QuoteSection>
             <Container>
                 <FlexWrapper align={'center'} direction={"column"} justify={"center"}>
-                    <S.QuoteWrapper>
-                        <S.IconQuoteWrapper className="up-quotes">
-                            <Icon iconId={"up_quotes"} viewBox={"0 0 26 21"}
-                                  height={"21px"}
-                                  width={"26px"}/>
-                        </S.IconQuoteWrapper>
+                        <S.QuoteWrapper>
+                            <S.IconQuoteWrapper className="up-quotes">
+                                <Icon iconId={"up_quotes"} viewBox={"0 0 26 21"}
+                                      height={"21px"}
+                                      width={"26px"}/>
+                            </S.IconQuoteWrapper>
 
-                        <S.Quote>
-                            {t('home.quote.text')}
-                        </S.Quote>
-                        <S.IconQuoteWrapper className="down-quotes">
-                            <Icon iconId={"up_quotes"} viewBox={"0 0 26 21"}
-                                  height={"21px"}
-                                  width={"26px"}/>
-                        </S.IconQuoteWrapper>
-                        <S.AuthorWrapper>
-                            <S.Author>
-                                {t('home.quote.author')}
-                            </S.Author>
-                        </S.AuthorWrapper>
-                    </S.QuoteWrapper>
+                            <S.Quote>
+                                {t('home.quote.text')}
+                            </S.Quote>
+                            <S.IconQuoteWrapper className="down-quotes">
+                                <Icon iconId={"up_quotes"} viewBox={"0 0 26 21"}
+                                      height={"21px"}
+                                      width={"26px"}/>
+                            </S.IconQuoteWrapper>
+                            <S.AuthorWrapper>
+                                <S.Author>
+                                    {t('home.quote.author')}
+                                </S.Author>
+                            </S.AuthorWrapper>
+                        </S.QuoteWrapper>
                 </FlexWrapper>
             </Container>
             <span></span>
